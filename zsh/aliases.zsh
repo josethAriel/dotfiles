3e6ede1366b6dd19e5e2aaf12777621bf41503b1
Joseth Ariel <arielbaquero.ab@gmail.com>
2023-01-17 01:03:56 -0500

    Add zsh (.zshenv and .zshrc)

---
+++ zsh/aliases.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+alias ll='lsd -lh --group-dirs=first'
+alias la='lsd -a --group-dirs=first'
+alias l='lsd --group-dirs=first'
+alias lla='lsd -lha --group-dirs=first'
+alias ls='lsd --group-dirs=first'
+alias cat='bat'
+alias code='code --force-device-scale-factor=1.5'
+alias nv='nvim'
+alias android-emulators='~/Android/Sdk/emulator/emulator -list-avds'
+alias android-emulator='~/Android/Sdk/emulator/emulator -avd $1'
+#alias connect='kitty +kitten ssh'
